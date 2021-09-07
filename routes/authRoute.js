31f2d37f426d0a44197cd6e1d7374dfb6f73c35a
dussan <dussan.freire@ucb.edu.bo>
2021-09-06 23:36:30 -0400

    new refactrs

---
+++ routes/authRoute.js
@@ -0,0 +1,113 @@
+const passport = require("passport");
+const { pool } = require("../config/dbConfig");
+const bcrypt = require("bcrypt");
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/dashboard");
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/users/login");
+}
+
+module.exports = (app) => {
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+  app.get("/users/register", checkAuthenticated, (req, res) => {
+    res.render("register");
+  });
+
+  app.get("/users/login", checkAuthenticated, (req, res) => {
+    res.render("login");
+  });
+
+  app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+    res.render("dashboard", { user: req.user.name });
+  });
+
+  app.get("/users/logout", (req, res) => {
+    req.logOut();
+    req.flash("success_msg", "Cerraste tu sesión");
+    res.redirect("/users/login");
+  });
+
+  app.post("/users/register", async (req, res) => {
+    let { name, email, password, password2 } = req.body;
+
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+      errors.push({ message: "Ingresa todos los campos." });
+    }
+
+    if (password.length < 6) {
+      errors.push({
+        message: "La contaseña no debe tener menos de 6 caracteres.",
+      });
+    }
+
+    if (password != password2) {
+      errors.push({ message: "Las contraseñas no coinciden." });
+    }
+
+    if (errors.length > 0) {
+      res.render("register", { errors });
+    } else {
+      // Form validation has passed
+      let hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
+
+      pool.query(
+        `SELECT * FROM users 
+                  WHERE email = $1`,
+        [email],
+        (err, results) => {
+          if (err) {
+            throw err;
+          }
+          console.log(results.rows);
+
+          if (results.rows.length > 0) {
+            errors.push({ message: "Correo electronico ya registrado." });
+            res.render("register", { errors });
+          } else {
+            pool.query(
+              `INSERT INTO users (name, email, password)
+                              VALUES ($1, $2, $3)
+                              RETURNING id, password`,
+              [name, email, hashedPassword],
+              (err, results) => {
+                if (err) {
+                  throw err;
+                }
+                console.log(results.rows);
+                req.flash(
+                  "success_msg",
+                  "Ya estas registrado. Por favor inicia sesión."
+                );
+                res.redirect("/users/login");
+              }
+            );
+          }
+        }
+      );
+    }
+  });
+
+  app.post(
+    "/users/login",
+    passport.authenticate("local", {
+      successRedirect: "/users/dashboard",
+      failureRedirect: "/users/login",
+      failureFlash: true,
+    })
+  );
+};
---
+++ services/passport.js
@@ -0,0 +1,73 @@
+const passport = require("passport");
+const flash = require("express-flash");
+const LocalStrategy = require("passport-local").Strategy;
+const { pool } = require("../config/dbConfig");
+const bcrypt = require("bcrypt");
+
+function initializePassport(passport) {
+  const authenticateUser = (email, password, done) => {
+    pool.query(
+      `SELECT * FROM users WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          const user = results.rows[0];
+
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) {
+              throw err;
+            }
+
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, {
+                message: "La contraseña es incorrecta",
+              });
+            }
+          });
+        } else {
+          return done(null, false, {
+            message: "Correo electronico no registrado",
+          });
+        }
+      }
+    );
+  };
+
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      authenticateUser
+    )
+  );
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser((id, done) => {
+    pool.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
+      if (err) {
+        throw err;
+      }
+      return done(null, results.rows[0]);
+    });
+  });
+}
+
+initializePassport(passport);
+
+module.exports = (app) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(flash());
+};
